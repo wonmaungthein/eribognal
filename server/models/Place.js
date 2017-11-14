48fb4d4ed272d522b9142e75c40bfb1397c93a9b
DwinaTech <diwina1993@hotmail.com>
2017-11-14 14:51:01 +0000

    Add category and status (#36)
    * added category and status to places model and added  also category and status select filed on add places form with some editting the form style
    
    * edit status title and removed unncesassry option
    
    * added validation to Places Schema and recorrected status title

--- server/models/Place.js
+++ server/models/Place.js
@@ -1,9 +1,33 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const statuses = {
+    values: ['Approved', 'Rejected', 'Pending'],
+    message: 'enum validator failed for path `{PATH}` with value `{VALUE}`'
+};
+
+const categories = {
+    values: ['Growing Project', 'Night Out', 'Shopping', 'Eating Out'],
+    message: 'enum validator failed for path `{PATH}` with value `{VALUE}`'
+}
+
 const schema = new Schema({
-    name: String,
-    description: String
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description field is required']
+    },
+    category: {
+        type: String,
+        enum: categories
+    },
+    status: {
+        type: String,
+        enum: statuses
+    }
 });
 
 const Place = mongoose.model('Place', schema);
