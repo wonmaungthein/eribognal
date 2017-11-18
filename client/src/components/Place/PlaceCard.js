0fc294d3851b64408c9e1c91e866d3d11cc89600
Etza <topimorita@gmail.com>
2017-11-18 08:21:01 +0000

    Applied design to Place Card and Place List (#41)
    * Working on placeCard and placelist desing
    
    * Working on creating a place lists with material-ui
    
    * Applied design to Place Card and Place List
    
    * Added Roboto Font to match with the design

--- client/src/components/Place/PlaceCard.js
+++ client/src/components/Place/PlaceCard.js
@@ -1,16 +1,59 @@
 import React from 'react';
+import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
+import List, { ListItem } from 'material-ui/List';
+
+
+
+const styles = ({
+    
+    listTitle: {
+        fontSize: 20,
+        color: "rgba(0,0,0,0.87)",
+        letterSpacing: 0,
+        lineHeight: 2.8,
+    },
+
+    listAddress: {
+        fontSize: 14,
+        color: "rgba(0,0,0,0.54)",
+        lineHeight: 2.1,
+    },
+
+    listDetails: {
+        fontSize: 14,
+        color: "rgba(0,0,0,0.54)",
+        lineHeight: 2.1,
+    },
+    listItem: {
+        display: "flex",
+        flexDirection: "column",
+        marginTop: -95,
+        paddingLeft: 35,
+    }
+
+});
+
 
 const PlaceCard = props => {
     const place = props.place;
     return (
+
         <div>
-            <ul>
-                <li> Name: {place.name} </li>
-                <li> Description: {place.description} </li>
-                <li> Category:   {place.category} </li>
-            </ul>
+            <Grid container spacing={24}>
+                <Grid item xs={12}>
+                        <ListItem style={styles.listItem}>
+                            <p style={styles.listTitle}> {place.name} </p>
+                            <p style={styles.listAddress}>{place.address} </p>
+                            <p style={styles.listDetails}> {place.description} </p>
+                        </ListItem>
+                </Grid>
+            </Grid>
         </div>
     )
-}
+};
 
-export default PlaceCard;
+
+
+export default withStyles(styles)(PlaceCard);
+
--- client/src/containers/PlaceList/PlaceList.js
+++ client/src/containers/PlaceList/PlaceList.js
@@ -2,7 +2,41 @@ import React from 'react';
 import PlaceCard from '../../components/Place/PlaceCard';
 import apiClient from '../../helpers/apiClient';
 import AddPlaceButton from '../../components/Place/AddPlaceButton'
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+import Avatar from 'material-ui/Avatar';
+import Card, { CardContent } from 'material-ui/Card'
 
+const styles = ({
+    listPlaces: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        alignSelf: "baseline",
+        paddingLeft: 10,
+        margin: 0,
+    },
+    listSeparator: {
+        color: "#E0E0E0",
+    },
+
+    listIcon: {
+        width: 40,
+        height: 40,
+        border: "none",
+        background: "transparent",
+        borderRadius: 0,
+    },
+    cardList: {
+        display: "flex",
+        flexDirection: "column",  
+        alignItems: "flex-start",
+        paddingLeft: 20,
+        marginLeft: 10,
+        paddingTop: 15,
+       
+    }
+});
 
 class PlacesList extends React.Component {
     constructor(props) {
@@ -22,18 +56,43 @@ class PlacesList extends React.Component {
     }
     render() {
         return (
-            <div>
+            <Grid container spacing={24} className="listPlaces" style={ 
+                {display: "flex",
+                flexDirection: "column",
+                alignItems: "stretch",
+                }}>
                 {
                     this.state.places.map((place) => {
                         return (
-                            <PlaceCard place={place} />
+                            <div>
+                                <Grid item xs={12}>
+                                    <Card className="cardList" style={styles.cardList}>
+                                        <Avatar>
+                                            <img src="../icon-growing-project.jpg" style={styles.listIcon} />
+                                        </Avatar>
+                                        <CardContent className="cardText">
+                                            <PlaceCard place={place} />
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Card className="cardList" style={styles.cardList}>
+                                        <Avatar>
+                                            <img src="../icon-local-drink.jpg" style={styles.listIcon} />
+                                        </Avatar>
+                                        <CardContent className="cardText">
+                                            <PlaceCard place={place} />
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            </div>
                         )
                     })
                 }
                 <AddPlaceButton />
-            </div>
+            </Grid>
         )
     }
 }
 
-export default PlacesList; 
+export default withStyles(styles)(PlacesList); 
