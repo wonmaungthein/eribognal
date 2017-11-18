a2553e1ef1959d6d1cc8d9bd91b5c28a62cc4e02
khaledkzy <khaledkzy@gmail.com>
2017-11-18 11:31:56 +0000

    Fix the the top header position (#44)

--- client/src/Themes.js
+++ client/src/Themes.js
@@ -1,19 +1,14 @@
-
-
 const Themes = theme => ({
-    root: {
-      flexGrow: 1,
-      marginTop: 30,
-    },
-    flex: {
-      flex: 1,
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
   },
   menuButton: {
-      marginLeft: -12,
-      marginRight: 20,
+    marginLeft: -12,
+    marginRight: 20,
   },
-  });
-  
+});
 
-  export default Themes;
-  
+export default Themes;
