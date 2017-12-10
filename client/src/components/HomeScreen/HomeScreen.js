f6d7cdd8c6b31c5cb6430d612ac09b4a9400789f
Yjohn <yfekadu2016@gmail.com>
2017-12-10 15:21:04 +0000

    Make all the homepage Clickable (#83)

--- client/src/components/HomeScreen/HomeScreen.js
+++ client/src/components/HomeScreen/HomeScreen.js
@@ -5,14 +5,19 @@ import { Link } from 'react-router-dom';
 const HomeScreen = () => {
     return <div>
         <div className="main">
-            <div className="overlay"></div>
             <div className="first-content">
+                <Link to="/questionnaire">
+                    <div className="firstOverlay"></div>
+                </Link>
                 <div className="question-header">
                     <Link to="/questionnaire">Questionnaire</Link>
                 </div>
                 <img className="img-control" src="home-screen-one-539431.jpg" alt="home screen one" />
             </div>
             <div className="second-content">
+                <Link to="/places">
+                    <div className="secondOverlay"></div>
+                </Link>
                 <div className="food-map-header">
                     <Link to="/places">Food map</Link>
                 </div>
