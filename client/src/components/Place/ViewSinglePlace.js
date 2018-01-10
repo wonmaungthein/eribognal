1b220fe35aa4c7ed2a6951f63c5f271510e1ebf4
Etza <topimorita@gmail.com>
2018-01-10 21:25:55 +0000

    Details (#97)
    * Got the latitude and longitude of the address selected by the user
    
    * Got the latitude and longitude of the address selected by the user
    
    * Saving coordinates into database
    
    * Solved bug in view single place
    
    * Fixed bug in view single place

--- client/src/components/Place/ViewSinglePlace.js
+++ client/src/components/Place/ViewSinglePlace.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import apiClient from '../../helpers/apiClient';
-import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
-import CategoryIcon from '../CategoryIcon/CategoryIcon'
 
 
 const styles = ({
@@ -20,9 +18,15 @@ const styles = ({
         margin: 20
     },
     listDetails: {
-        fontSize: 18,
+        fontSize: 15,
         color: "rgba(0,0,0,0.54)",
         marginTop: 10,
+    },
+    listCategories: {
+        fontSize: 17,
+        color: "rgba(0,0,0,0.54)",
+        marginTop: 10,
+        fontWeight: "bold",
     },
     listTitle: {
         fontSize: 20,
@@ -75,13 +79,12 @@ class viewPlace extends React.Component {
             <div>
                 <Grid container spacing={24} style={styles.gridStyle}>
                     <Grid item xs={4}>
-                        <CategoryIcon category={place.category} style={{ textAlign: 'center' }} />
                     </Grid>
                     <Grid item xs={8} style={{ paddingTop: 0 }}>
                         <div>
-                            <p style={styles.listAddress}>{place.name} </p>
+                            <p style={styles.listTitle}>{place.name} </p>
                             <p style={styles.listDetails}> {place.description} </p>
-                            <p style={styles.listDetails}> {place.category} </p>
+                            <p style={styles.listCategories}> {place.category} </p>
                             {this._renderAddress(place)}
                         </div>
                     </Grid>
