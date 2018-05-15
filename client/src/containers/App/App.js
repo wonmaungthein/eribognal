13321208413f94ca44019c695de6874cb6121293
Won Maung Thein <wonmaungthein@gmail.com>
2018-05-15 14:45:59 +0100

    fixed the link which was not correctly inported

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react'
-import './App.css'
-import Menu from '../../components/Menu/Menu'
-import About from '../../components/About/About'
-import viewPlace from '../../components/Place/ViewSinglePlace'
-import PlacesList from '../PlaceList/PlaceList'
-import AddPlaceForm from '../PlaceList/AddPlaceForm'
-import Questions from '../Questionnaire/Questionnaire'
-import { withStyles } from 'material-ui/styles'
-import Grid from 'material-ui/Grid'
-import Themes from '../../Themes'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import HomeScreen from '../../components/HomeScreen/HomeScreen'
-import PreHomeScreen from '../../components/PreHomeScreen/PreHomeScreen'
+import React, { Component } from "react";
+import "./App.css";
+import Menu from "../../components/Menu/Menu";
+import About from "../../components/About/About";
+import viewPlace from "../../components/Place/ViewSinglePlace";
+import PlacesList from "../PlaceList/PlaceList";
+import AddPlaceForm from "../PlaceList/AddPlaceForm";
+import Questions from "../Questionnaire/Questions";
+import { withStyles } from "material-ui/styles";
+import Grid from "material-ui/Grid";
+import Themes from "../../Themes";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeScreen from "../../components/HomeScreen/HomeScreen";
+import PreHomeScreen from "../../components/PreHomeScreen/PreHomeScreen";
 
 class App extends Component {
-  render () {
-    const { classes } = this.props
+  render() {
+    const { classes } = this.props;
     return (
       <Router>
-        <div className='containerFlex'>
-          <div className='containerApp'>
+        <div className="containerFlex">
+          <div className="containerApp">
             <div className={classes.root}>
               <Menu />
-              <Route exact path='/' component={PreHomeScreen} />
-              <Route exact path='/Home' component={HomeScreen} />
-              <Route path='/about' component={About} />
-              <Route path='/questionnaire' component={Questions} />
-              <Route exact path='/places/:placeId' component={viewPlace} />
-              <Route exact path='/places' component={PlacesList} />
-              <Route path='/new-place' component={AddPlaceForm} />
+              <Route exact path="/" component={PreHomeScreen} />
+              <Route exact path="/Home" component={HomeScreen} />
+              <Route path="/about" component={About} />
+              {/* <Route path="/questionnaire" component={Questions} /> */}
+              <Route path="/questionnaire" component={Questions} />
+              <Route exact path="/places/:placeId" component={viewPlace} />
+              <Route exact path="/places" component={PlacesList} />
+              <Route path="/new-place" component={AddPlaceForm} />
             </div>
           </div>
         </div>
       </Router>
-    )
+    );
   }
 }
 
-export default withStyles(Themes)(App)
+export default withStyles(Themes)(App);
